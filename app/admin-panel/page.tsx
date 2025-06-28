32114309ca112f2a231ce8e3600a4da6e0727dae
Nikita <nikitaloginovxxx1986@gmail.com>
2025-06-28 19:03:36 +0300

    update

--- app/admin-panel/page.tsx
+++ app/admin-panel/page.tsx
@@ -1,28 +1,38 @@
 import { AdminPanel } from "@/components/shared/admin-panel";
 import { Container } from "@/components/shared/container";
-import { cookies } from "next/headers";
 import { notFound } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default async function AdminPanelPage() {
+export default function AdminPanelPage() {
 
-    const cookieStore = await cookies()
-    const token = cookieStore.get("jwt")?.value
+    // const cookieStore = await cookies()
+    // const token = cookieStore.get("jwt")?.value
 
-    if (!token) {
-        notFound()
-    }
+    // if (!token) {
+    //     notFound()
+    // }
 
-    try {
-        const payloadBase64 = token.split(".")[1]
-        const decodedPayload = JSON.parse(
-            Buffer.from(payloadBase64, "base64").toString("utf-8")
-        )
+    // try {
+    //     const payloadBase64 = token.split(".")[1]
+    //     const decodedPayload = JSON.parse(
+    //         Buffer.from(payloadBase64, "base64").toString("utf-8")
+    //     )
 
-        if (decodedPayload.role !== "ADMIN") {
-            notFound()
-        }
-    } catch (err) {
-        console.error(err)
+    //     if (decodedPayload.role !== "ADMIN") {
+    //         notFound()
+    //     }
+    // } catch (err) {
+    //     console.error(err)
+    //     notFound()
+    // }
+
+    const [role, setRole] = useState("")
+
+    useEffect(() => {
+        setRole(localStorage.getItem("role") || "")
+    }, [])
+
+    if (role !== "ADMIN") {
         notFound()
     }
 
