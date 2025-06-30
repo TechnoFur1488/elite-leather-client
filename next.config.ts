b5b47433eb0668204dcde1438022cf57f35d347f
NikitaSilniy <nikitaloginovxxx1986@gmail.com>
2025-06-30 21:44:05 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
-      "elite-leather-client.vercel.app"
+      "elite-leather-server-production.up.railway.app"
     ]
   }
 };
